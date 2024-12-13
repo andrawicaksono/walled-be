3efba912b710ab019c53467f7489fe31a9451540
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-13 11:08:57 +0700

    fix: add transaction validation

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { transactionController, authMiddleware } = require("../container");
+const { validate } = require("../middlewares/validation");
+const {
+  createTransactionSchema,
+} = require("../validators/transactionValidator");
 
 router.post(
   "/",
   authMiddleware.verifyToken,
+  validate(createTransactionSchema),
   transactionController.createTransaction
 );
 
--- src/validators/transactionValidator.js
+++ src/validators/transactionValidator.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const createTransactionSchema = Joi.object({
-  type: Joi.string().min(1).max(1).required(),
+  type: Joi.string().valid("c", "d").required(),
   from_to: Joi.string().min(3).max(10).required(),
   description: Joi.string().max(30),
   amount: Joi.number().required(),
