4c46d9de250b4d5ecab169693f71e245ae92f59f
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-17 00:25:28 +0700

    fix: change migrate

--- migrations/migrate.sql
+++ migrations/migrate.sql
@@ -1,29 +1,30 @@
-DROP TABLE IF EXISTS transactions;
-DROP TABLE IF EXISTS users;
+drop table if exists transactions;
+drop table if exists users;
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  full_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(255) NOT NULL,
-  account_no VARCHAR(255) NOT NULL UNIQUE,
-  balance double precision NOT NULL DEFAULT 0,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  full_name VARCHAR(255) not null,
+  email VARCHAR(255) not null unique,
+  password VARCHAR(255) not NULL,
+  phone_number VARCHAR(255),
+  avatar_url VARCHAR(255),
+  account_no VARCHAR(255) not null unique,
+  balance double precision not null default 0,
+  created_at TIMESTAMP not null default NOW(),
+  updated_at TIMESTAMP not null default NOW(),
   deleted_at TIMESTAMP
 );
 
-CREATE TABLE transactions(
+CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  type char(1) NOT NULL,
-  from_to VARCHAR(255) NOT NULL,
+  user_id INTEGER not null,
+  type char(1) not null,
+  from_to VARCHAR(255) not null,
   description VARCHAR(255),
-  amount double precision NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  amount double precision not null,
+  created_at TIMESTAMP not null default NOW(),
+  updated_at TIMESTAMP not null default NOW(),
   deleted_at TIMESTAMP
 );
 
-ALTER TABLE transactions ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE transactions ADD FOREIGN KEY (user_id) REFERENCES users (id);
