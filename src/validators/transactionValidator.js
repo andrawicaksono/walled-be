33e4f95f8c13f87262a88d742445154f21774890
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-18 08:35:18 +0700

    fix: validation

--- src/validators/transactionValidator.js
+++ src/validators/transactionValidator.js
@@ -4,7 +4,7 @@ const createTransactionSchema = Joi.object({
   type: Joi.string().valid("c", "d").required(),
   from_to: Joi.string().min(3).max(20).required(),
   description: Joi.string().max(30),
-  amount: Joi.number().required(),
+  amount: Joi.number().min(1).required(),
 });
 
 module.exports = {
