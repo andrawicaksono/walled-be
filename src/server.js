9e13ae2947be08bfccd46725fb40d088ee3504e1
Andra Wicaksono <93083422+andrawicaksono@users.noreply.github.com>
2025-04-10 12:38:44 +0700

    fix: allow all origin using cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const config = require("./config");
 const routes = require("./routes");
 const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
